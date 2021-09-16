a98cc055985126c7469a1b4ad72f22cacc03a590
Shulabh-968026 <58615131+Shulabh-968026@users.noreply.github.com>
2021-09-16 14:47:00 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -132,3 +132,6 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'static/images')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
